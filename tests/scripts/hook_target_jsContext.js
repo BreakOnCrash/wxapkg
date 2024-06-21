5a6087ea709d432431b81f450040d600b5fbd40d
巴斯.zznQ <admin@imipy.com>
2024-06-21 15:14:43 +0800

    Update hook_target_jsContext.js

--- tests/scripts/hook_target_jsContext.js
+++ tests/scripts/hook_target_jsContext.js
@@ -1,3 +1,4 @@
+// https://webkit.org/blog/13936/enabling-the-inspection-of-web-content-in-apps/
 ['WKWebView', 'JSContext'].forEach(
     clazz => ObjC.chooseSync(ObjC.classes[clazz]).forEach(
         v => v.setInspectable_(ptr(1))
